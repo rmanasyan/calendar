aef7c1169497568c1b48e54f31ab4c66bb1b128c
Roman Manasyan <rmanasyan@gmail.com>
2023-09-16 15:22:15 +0300

    chore: ng add @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+    ]),
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'calendar-e59e2',
+    appId: '1:275384391887:web:669f784d086c10d70fbfba',
+    storageBucket: 'calendar-e59e2.appspot.com',
+    apiKey: 'AIzaSyBrbHSdIKyZohpr3a6ORoIjtsA5pzy49mo',
+    authDomain: 'calendar-e59e2.firebaseapp.com',
+    messagingSenderId: '275384391887',
+  },};
